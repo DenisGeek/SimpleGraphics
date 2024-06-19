203309e4d025ff4e06a1155e762726096946b52d
Denis Anokhin <denis.anokhin.anthill@gmail.com>
2024-06-19 19:26:38 +0300

    Draw link lines controls in order primitives was selected Ctrl+L, unlink in selection order Ctrl+U

--- SimpleGraphicsControls/CustomControls/SGControlBox.cs
+++ SimpleGraphicsControls/CustomControls/SGControlBox.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
+using System.DirectoryServices.ActiveDirectory;
 using System.Drawing;
 using System.Linq;
 using System.Security.Policy;
@@ -27,7 +28,12 @@ namespace SGTestingApp.CustomControls
         /// <summary>
         /// <inheritdoc cref="_isModePrimitivesLinkSelection"/>
         /// </summary>
-        public bool IsModePrimitivesLinkSelection { get => _isModePrimitivesLinkSelection;}
+        public bool IsModePrimitivesLinkSelection { get => _isModePrimitivesLinkSelection; }
+
+        /// <summary>
+        /// List of all <inheritdoc cref="SGPLinkLine"/>
+        /// </summary>
+        private List<SGPLinkLine> _LinkedLines = new List<SGPLinkLine>();
 
         #endregion Fields and Properties
 
@@ -38,8 +44,10 @@ namespace SGTestingApp.CustomControls
         {
             InitializeComponent();
         }
-        
+
         #region Methods
+
+        #region Select Primitives
 
         /// <summary>
         /// If pressed Control key then activate selection mode
@@ -49,11 +57,26 @@ namespace SGTestingApp.CustomControls
         {
             base.OnKeyDown(e);
 
+            // Select
             if (e.Control)
             {
                 _isModePrimitivesLinkSelection = true;
-                //_selectedPrimitives.Clear();
             }
+
+            // Link
+            if (e.Control && e.KeyCode == Keys.L)
+            {
+                LinkedLinesAddAllSelectedSGP(_selectedPrimitives);
+                Invalidate(true);
+            }
+
+            //Unlink
+            if (e.Control && e.KeyCode == Keys.U)
+            {
+                LinkedLinesAddRemoveSelectedSGP(_selectedPrimitives);
+                Invalidate(true);
+            }
+
         }
 
         /// <summary>
@@ -63,11 +86,10 @@ namespace SGTestingApp.CustomControls
         protected override void OnKeyUp(KeyEventArgs e)
         {
             base.OnKeyUp(e);
-
+            var k = e.KeyCode;
             //if (e.Control)
             {
                 _isModePrimitivesLinkSelection = false;
-                // DO STUFF
                 _selectedPrimitives.Clear();
             }
 
@@ -93,6 +115,91 @@ namespace SGTestingApp.CustomControls
         /// <returns></returns>
         public bool SelectedPrimitivesCheck(SGPBase item)
             => _selectedPrimitives.Contains(item);
+
+        #endregion Select Primitives
+
+        #region Manage Link Line
+
+        /// <summary>
+        /// Get all <see cref="SGPLinkLine"/> in <see cref="_LinkedLines"/>, which contains tis pair of <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        /// <returns>all <see cref="SGPLinkLine"/> with pair <inheritdoc cref="SGPBase" path="/summary/inner"/> </returns>
+        private List<SGPLinkLine> LinkedLinesGetAllPairSGP(SGPBase first, SGPBase second)
+            => _LinkedLines.Where(x => (x.FirstSGP == first || x.FirstSGP == second) && (x.SecondSGP == first || x.SecondSGP == second)).ToList();
+
+        /// <summary>
+        /// Do <see cref="_LinkedLines"/> contains tis pair of <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        /// <returns><typeparamref name="true"/> if contains</returns>
+        private bool LinkedLinesContainsPairSGP(SGPBase first, SGPBase second)
+            => LinkedLinesGetAllPairSGP(first, second).Count() > 0;
+
+        /// <summary>
+        /// Add into <see cref="_LinkedLines"/> new <see cref="SGPLinkLine"/> with pair <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        private void LinkedLinesAddPairSGP(SGPBase first, SGPBase second)
+        {
+            if (!LinkedLinesContainsPairSGP(first, second))
+            {
+                var newLinkLine = new SGPLinkLine() { FirstSGP = first, SecondSGP = second, Parent = this };
+                _LinkedLines.Add(newLinkLine);
+
+                Controls.Add(newLinkLine);
+            }
+        }
+
+        /// <summary>
+        /// Remove from <see cref="_LinkedLines"/> all <see cref="SGPLinkLine"/> with pair <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        private void LinkedLinesRemovePairSGP(SGPBase first, SGPBase second)
+        {
+            var allPairs = LinkedLinesGetAllPairSGP(first, second);
+            if (allPairs.Count > 0)
+            {
+                var items4Remove = _LinkedLines.Where(x => allPairs.Contains(x)).ToList();
+                
+                _LinkedLines.RemoveAll(x => items4Remove.Contains(x));
+                
+                items4Remove.ForEach(x => { Controls.Remove(x); x.Dispose(); });
+            }
+        }
+
+        /// <summary>
+        /// Add all selected SGP in order selection to <see cref="_LinkedLines"/>
+        /// </summary>
+        /// <param name="selected"></param>
+        private void LinkedLinesAddAllSelectedSGP(List<SGPBase> selected)
+        {
+            if (selected.Count <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < selected.Count - 1; i++)
+            {
+                LinkedLinesAddPairSGP(selected[i], selected[i + 1]);
+            }
+        }
+
+
+        /// <summary>
+        /// Remove all selected SGP in order selection from <see cref="_LinkedLines"/>
+        /// </summary>
+        /// <param name="selected"></param>
+        private void LinkedLinesAddRemoveSelectedSGP(List<SGPBase> selected)
+        {
+            if (selected.Count <= 1)
+            {
+                return;
+            }
+
+            for (int i = 0; i < selected.Count - 1; i++)
+            {
+                LinkedLinesRemovePairSGP(selected[i], selected[i + 1]);
+            }
+        }
+
+        #endregion Manage Link Line
 
         #endregion Methods
     }
---
+++ SimpleGraphicsControls/CustomControls/SimpleGraphicsPrimitives/SGPLinkLine.cs
@@ -0,0 +1,150 @@
+﻿using SGTestingApp.CustomControls.SimpleGraphicsPrimitives;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Drawing2D;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SGTestingApp.CustomControls.SimpleGraphicsPrimitives
+{
+    /// <summary>
+    /// Link line <inheritdoc cref="SGPBase" path="/summary/inner"/>
+    /// </summary>
+    [ToolboxItem(false)]
+    public partial class SGPLinkLine : Control
+    {
+        #region Fields and Properties
+
+        /// <summary>
+        /// First linked <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        public SGPBase? FirstSGP { get; set; }
+        /// <summary>
+        /// Second linked <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        public SGPBase? SecondSGP { get; set; }
+
+        /// <summary>
+        /// Link line pen
+        /// </summary>
+        protected Pen PenLinkLine { get; set; } = new(Color.OrangeRed);
+
+        #endregion Fields and Properties
+
+        /// <summary>
+        /// Constructor, using <see cref="InitializeComponent"/> - <inheritdoc cref="InitializeComponent"/>
+        /// </summary>
+        public SGPLinkLine()
+        {
+            InitializeComponent();
+            PenLinkLine.DashStyle = DashStyle.Dash;
+            PenLinkLine.Width = 3;
+
+            Location = new Point(0, 0);
+            Size = new System.Drawing.Size(75, 75);
+        }
+
+        #region Methods
+
+        /// <summary>
+        /// <inheritdoc cref="SGPLinkLine"/> fill and draw, when <inheritdoc cref="Control.OnPaint" path="/summary"/>
+        /// </summary>
+        /// <param name="pe"><inheritdoc cref="Control.OnPaint" path="/param[@name='e']"/></param>
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+
+
+            if (FirstSGP == null || SecondSGP == null)
+            {
+                return;
+            }
+
+            SetControlPosition(pe.Graphics);
+            SetControlBounds();
+
+            BringToFront();
+
+            // Create points that define line.
+            Point firstSGPCenter = GetSGPCenter(FirstSGP);
+            Point secondSGPCenter = GetSGPCenter(SecondSGP);
+
+            // Draw an triangle in the rectangle represented by the control.
+            Point bg = new Point(firstSGPCenter.X - Location.X, firstSGPCenter.Y - Location.Y);
+            Point nd = new Point(secondSGPCenter.X - Location.X, secondSGPCenter.Y - Location.Y);            
+            pe.Graphics.DrawLine(PenLinkLine, bg, nd);
+        }
+
+        /// <summary>
+        /// Get center coordinates of <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        /// <param name="item"></param>
+        /// <returns></returns>
+        private Point GetSGPCenter(SGPBase item)
+            => new Point(item.Location.X + item.Width / 2, item.Location.Y + item.Height / 2);
+
+        /// <summary>
+        /// Set control position bases on centres of <see cref="FirstSGP"/> and <see cref="SecondSGP"/>
+        /// </summary>
+        private void SetControlPosition(Graphics g)
+        {
+            if (FirstSGP == null || SecondSGP == null)
+            {
+                return;
+            }
+
+            Point firstSGPCenter = GetSGPCenter(FirstSGP);
+            Point secondSGPCenter = GetSGPCenter(SecondSGP);
+
+            Point newLocation = new(0, 0);
+            newLocation.X = firstSGPCenter.X < secondSGPCenter.X ? firstSGPCenter.X : secondSGPCenter.X;
+            newLocation.Y = firstSGPCenter.Y < secondSGPCenter.Y ? firstSGPCenter.Y : secondSGPCenter.Y;
+            Location = newLocation;
+
+            Width =  firstSGPCenter.X > secondSGPCenter.X ? firstSGPCenter.X : secondSGPCenter.X - newLocation.X;
+            Height = firstSGPCenter.Y > secondSGPCenter.Y ? firstSGPCenter.Y : secondSGPCenter.Y - newLocation.Y;
+
+            g.DrawRectangle(PenLinkLine, new Rectangle(new Point(0, 0), new Size(this.Size.Width - 1, this.Size.Height - 1)));
+        }
+
+        /// <summary>
+        /// Set control bouds based on control positions and centres of <see cref="FirstSGP"/> and <see cref="SecondSGP"/>
+        /// </summary>
+        private void SetControlBounds()
+        {
+            if (FirstSGP == null || SecondSGP == null)
+            {
+                return;
+            }
+
+            Point firstSGPCenter = GetSGPCenter(FirstSGP);
+            Point secondSGPCenter = GetSGPCenter(SecondSGP);
+
+            Point bg = new Point(firstSGPCenter.X - Location.X, firstSGPCenter.Y - Location.Y);
+            Point nd = new Point(secondSGPCenter.X - Location.X, secondSGPCenter.Y - Location.Y);
+
+            // Control edge
+            var curvePath = new System.Drawing.Drawing2D.GraphicsPath();
+
+            // Create points that define triangle edge polygon.
+            Point point1 = new Point(bg.X - 2, bg.Y - 2);
+            Point point2 = new Point(nd.X - 2, nd.Y - 2);
+            Point point3 = new Point(nd.X + 2, nd.Y + 2);
+            Point point4 = new Point(bg.X + 2, bg.Y + 2);
+            Point[] curvePoints = { point1, point2, point3, point4 };
+
+            // Define control edge
+            curvePath.AddPolygon(curvePoints);
+
+            // Set control region
+            this.Region = new Region(curvePath);
+        }
+
+        #endregion Methods
+    }
+}

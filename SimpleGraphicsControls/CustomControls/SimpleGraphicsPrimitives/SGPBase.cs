28b6db93dd10a36d50ffd9114734e8881ce43de1
Denis Anokhin <denis.anokhin.anthill@gmail.com>
2024-06-19 05:44:48 +0300

    SGPBase clear and comment code

--- SimpleGraphicsControls/CustomControls/SimpleGraphicsPrimitives/SGPBase.cs
+++ SimpleGraphicsControls/CustomControls/SimpleGraphicsPrimitives/SGPBase.cs
@@ -7,70 +7,121 @@ namespace SGTestingApp.CustomControls.SimpleGraphicsPrimitives
     /// </summary>
     public partial class SGPBase : Control
     {
-        //public Color FillColor { get => _fillColor; set { _fillColor = value; _fillBrush = new SolidBrush(Color.FromArgb(0, value)); Invalidate(); } }
+        #region Fields and Properties
+
+        /// <summary>
+        /// <inheritdoc cref="_fillColor"/>
+        /// </summary>
         public Color FillColor { get => _fillColor; set { _fillColor = value; _fillBrush = new SolidBrush(value); Invalidate(); } }
+        /// <summary>
+        /// Fill color for <inheritdoc cref="SGPBase" path="/summary/inner"/>, needs for demonstrte color in editor
+        /// </summary>
         private Color _fillColor = Color.Green;
+        /// <summary>
+        /// Colored brush for <inheritdoc cref="SGPBase" path="/summary/inner"/>, using in OnPaint
+        /// </summary>
         protected Brush _fillBrush = Brushes.Green;
 
-        public Color BorderColor { get => _borderColor; set { _borderColor = value; _borderPen = new (value); Invalidate(); } }
+        /// <summary>
+        /// <inheritdoc cref="_borderColor"/>
+        /// </summary>
+        public Color BorderColor { get => _borderColor; set { _borderColor = value; _borderPen = new(value); Invalidate(); } }
+        /// <summary>
+        /// Border color for <inheritdoc cref="SGPBase" path="/summary/inner"/>, needs for demonstrte color in editor
+        /// </summary>
         private Color _borderColor = Color.Blue;
+        /// <summary>
+        /// Colored pen for <inheritdoc cref="SGPBase" path="/summary/inner"/>, using in OnPaint
+        /// </summary>
         protected Pen _borderPen = Pens.Blue;
 
+        /// <summary>
+        /// <inheritdoc cref="_borderHighlightWidth"/>
+        /// </summary>
         public int BorderHighlightWidth { get => _borderHighlightWidth; set { _borderHighlightWidth = value; _borderPenHighlight.Width = value; Invalidate(); } }
+        /// <summary>
+        /// Border highlight width
+        /// </summary>
         private int _borderHighlightWidth = 3;
-        public Color BorderHighlightColor { get => _borderColorHighlight; set { _borderColorHighlight = value; _borderPenHighlight = new(value);  Invalidate(); } }
+        /// <summary>
+        /// <inheritdoc cref="_borderColorHighlight"/>
+        /// </summary>
+        public Color BorderHighlightColor { get => _borderColorHighlight; set { _borderColorHighlight = value; _borderPenHighlight = new(value); Invalidate(); } }
+        /// <summary>
+        /// Border highlight color for <inheritdoc cref="SGPBase" path="/summary/inner"/>, needs for demonstrte color in editor
+        /// </summary>
         private Color _borderColorHighlight = Color.Violet;
+        /// <summary>
+        /// Colored pen for highlight <inheritdoc cref="SGPBase" path="/summary/inner"/>, using in OnPaint
+        /// </summary>
         protected Pen _borderPenHighlight = Pens.Violet;
 
+        /// <summary>
+        /// border pen, depends on mouse hover
+        /// </summary>
         protected Pen _borderPenCurrent = Pens.Blue;
 
-        protected bool _isMouseHover = false;
-        protected bool _isDrag = false;
-        protected bool _isResize = false;
+        /// <summary>
+        /// Flag abut mouse over the <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        private bool _isMouseHover = false;
 
-        protected MouseButtons _mouseButton = MouseButtons.None;
-        protected Point _mousePosition = new (0,0);
+        /// <summary>
+        /// Pressed mouse buttons over the <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        private MouseButtons _mouseButton = MouseButtons.None;
 
+        /// <summary>
+        /// initial mouse position when <inheritdoc cref="_mouseButton"/>
+        /// </summary>
+        private Point _mousePosition = new(0, 0);
+
+        #endregion Fields and Properties
+
+        /// <summary>
+        /// Constructor, using <see cref="InitializeComponent"/> - <inheritdoc cref="InitializeComponent"/>
+        /// </summary>
         public SGPBase()
         {
             InitializeComponent();
         }
 
-        //protected override void OnPaintBackground(PaintEventArgs pe)
-        //{
-        //    var transparentColor = Color.FromArgb(0, this.BackColor);
-        //    //pe.Graphics.Clear(transparentColor);
+        #region Mouse Enter Leave (border Pen Highlight)
 
-        //    var transparentBruch = new SolidBrush(transparentColor);
-
-        //    // Create a rectangle that represents the size of the control, minus 1 pixel.
-        //    var area = new Rectangle(new Point(0, 0), new Size(this.Size.Width - 1, this.Size.Height - 1));
-
-        //    // Draw an rectangle in the rectangle represented by the control.
-        //    pe.Graphics.FillRectangle(transparentBruch, area);
-        //}
+        /// <summary>
+        /// Change border pen, depends on mouse hover, while <inheritdoc cref="Control.OnPaint"/>
+        /// </summary>
+        /// <param name="pe"></param>
         protected override void OnPaint(PaintEventArgs pe)
         {
-            _borderPenCurrent = _isMouseHover ? _borderPenHighlight : _borderPen;
-            if (_isMouseHover)
+            // Call the method of the base class.
+            base.OnPaint(pe);
 
-                base.OnPaint(pe);
+            _borderPenCurrent = _isMouseHover ? _borderPenHighlight : _borderPen;
         }
 
-        #region Mouse Enter Leave
-
+        /// <summary>
+        /// Detect moment when mouse enter to control, while <inheritdoc cref="Control.OnMouseEnter"/>
+        /// </summary>
+        /// <param name="e"><inheritdoc cref="Control.OnMouseEnter" path="/param[@name='e']"/></param>
         protected override void OnMouseEnter(EventArgs e)
         {
-            base.OnEnter(e);
+            // Call the method of the base class.
+            base.OnMouseEnter(e);
 
             _isMouseHover = true;
 
             Invalidate();
         }
 
+        /// <summary>
+        /// Detect moment when mouse leave control, while <inheritdoc cref="Control.OnMouseLeave"/>
+        /// </summary>
+        /// <param name="e"><inheritdoc cref="Control.OnMouseLeave" path="/param[@name='e']"/></param>
         protected override void OnMouseLeave(EventArgs e)
         {
-            base.OnLeave(e);
+            // Call the method of the base class.
+            base.OnMouseLeave(e);
 
             _isMouseHover = false;
             _mouseButton = MouseButtons.None;
@@ -78,45 +129,57 @@ namespace SGTestingApp.CustomControls.SimpleGraphicsPrimitives
             Invalidate();
         }
 
-        #endregion Mouse Enter Leave
+        #endregion Mouse Enter Leave (border Pen Highlight)
 
+        #region Drag or resize
 
+        /// <summary>
+        /// Detect moment when user pressed mouse button over control and take initial mousePosition, while <inheritdoc cref="Control.OnMouseDown"/>
+        /// </summary>
+        /// <param name="e"><inheritdoc cref="Control.OnMouseDown" path="/param[@name='e']"/></param>
         protected override void OnMouseDown(MouseEventArgs e)
         {
             base.OnMouseDown(e);
 
             _mouseButton = e.Button;
-            _mousePosition = new (e.X, e.Y);
+            _mousePosition = new(e.X, e.Y);
         }
 
+        /// <summary>
+        /// Detect moment when user relse mouse button over control, while <inheritdoc cref="Control.OnMouseUp"/>
+        /// </summary>
+        /// <param name="e"><inheritdoc cref="Control.OnMouseUp" path="/param[@name='e']"/></param>
         protected override void OnMouseUp(MouseEventArgs e)
         {
+            // Call the method of the base class.
             base.OnMouseUp(e);
 
             _mouseButton = MouseButtons.None;
-
-            //_ = e.Button switch
-            //{
-            //    MouseButtons.Left => _isDrag = false,
-            //    MouseButtons.Right => _isResize = false,
-            //    _ => false,
-            //};
         }
 
+        /// <summary>
+        /// Drag or resize <inheritdoc cref="SGPBase" path="/summary/inner"/>
+        /// </summary>
+        /// <param name="e"></param>
         protected override void OnMouseMove(MouseEventArgs e)
         {
+            // Call the method of the base class.
             base.OnMouseMove(e);
-
-            Point mousePositionNew = new(e.X, e.Y);
-
+            
+            // Drag simple graphics primitives control
             if (_mouseButton == MouseButtons.Left)
             {
+                Point mousePositionNew = new(e.X, e.Y);
                 Point newLocation = new(Location.X + mousePositionNew.X - _mousePosition.X, Location.Y + mousePositionNew.Y - _mousePosition.Y);
+                
                 Location = newLocation;
             }
 
+            // Resize simple graphics primitives control
             if (_mouseButton == MouseButtons.Right)
             {
+                Point mousePositionNew = new(e.X, e.Y);
+
                 Height = Height + mousePositionNew.Y - _mousePosition.Y;
                 Width = Width + mousePositionNew.X - _mousePosition.X;
 
@@ -124,5 +187,7 @@ namespace SGTestingApp.CustomControls.SimpleGraphicsPrimitives
                 Invalidate();
             }
         }
+
+        #endregion Drag or resize
     }
 }

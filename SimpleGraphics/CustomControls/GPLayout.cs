b0171dae727adf350b147b76b61d93c22b54cb76
Denis Anokhin <denis.anokhin.anthill@gmail.com>
2024-06-20 10:06:43 +0300

    Init develop version2

---
+++ SimpleGraphics/CustomControls/GPLayout.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+using SimpleGraphics.GraphicPrimitives;
+
+namespace SimpleGraphics.CustomControls
+{
+    /// <summary>
+    /// Layout of <inheritdoc cref="GPBase" path="/summary/name"/>
+    /// </summary>
+    public partial class GPLayout : Control
+    {
+        public GPLayout()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnPaint(PaintEventArgs pe)
+        {
+            base.OnPaint(pe);
+        }
+    }
+}
